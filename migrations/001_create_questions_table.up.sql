1b6a56c9f806c8972088ea7d9cfe1486dc73b21e
Shahin <shahin.bayat@exaring.de>
2024-03-25 15:21:09 +0100

    feat: add migrations

---
+++ migrations/001_create_questions_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE
+  IF NOT EXISTS questions (
+    id SERIAL PRIMARY KEY,
+    question TEXT NOT NULL,
+    question_key VARCHAR(50) UNIQUE NOT NULL,
+    question_number VARCHAR(50) NOT NULL,
+    has_image BOOLEAN NOT NULL DEFAULT FALSE,
+    file_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP 
+  );
